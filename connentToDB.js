4d3af7d1122be6f0fffaf34b154f6b8714fa27e7
Gokul Gopi <gokulg1412@gmail.com>
2022-12-02 11:30:50 +0530

    fix: depecrated warnings because of mongoose

--- connentToDB.js
+++ connentToDB.js
@@ -1,22 +1,17 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGO_URI}/jam`,
-            {
-                useUnifiedTopology: true,
-                useNewUrlParser: true,
-            });
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/jam`, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
 
-        console.log(`MongoDB Connected`);
-    }
-    catch (error) {
-        console.log(`Error: ${error.message}`);
-    }
-
-}
-
-
+    console.log(`MongoDB Connected`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+};
 
 module.exports = connectToMongo;
